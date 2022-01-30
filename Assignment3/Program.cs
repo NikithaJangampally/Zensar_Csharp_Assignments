a10ed4f8272e99761549b6a7e1e65176dfc689d4
NikithaJangampally <61389118+NikithaJangampally@users.noreply.github.com>
2022-01-30 16:58:51 +0530

    Add files via upload

---
+++ Assignment3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 5. Write a program in C# Sharp to create a nested structure to store two data for an employee in an array. 
+
+Test Data:
+
+Create a nested structure and store data in an array :                              
+-------------------------------------------------------                          
+Name of the employee : H.Rana                                                    
+Input day of the birth : 05                                                      
+Input month of the birth : 02                                                    
+Input year for the birth : 58                                                    
+                                                                                 
+Name of the employee : S.Mathur                                                  
+Input day of the birth : 04                                                      
+Input month of the birth : 08                                                    
+Input year for the birth : 59 
+
+  */
+
+namespace Assignment3
+{
+    class Nested_Structure
+    {
+        //employee is a structure of members eName and DOB
+        struct employee
+        {
+            public string eName;
+            public DOB Date;
+        }
+        //DOB is a nested structure of employee
+        struct DOB
+        {
+            public int Date;
+            public int Month;
+            public int Year;
+        }
+        static void Main(string[] args)
+        {
+
+            int d, m, y;
+            string s;
+
+            Console.WriteLine("Create a nested structure and store data in an array :");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Enter Total no of employee you want to enter");
+            int t = Convert.ToInt32(Console.ReadLine());
+            employee[] e = new employee[t];
+
+            for (int i = 0; i < 2; i++)
+            {
+                //Initializing the data of name, day, month and year of Date of Birth in structure
+                Console.Write("Name of the employee : ");
+                s = Console.ReadLine();
+                e[i].eName = s;
+
+                Console.Write("Enter day of the birth : ");
+                d = Convert.ToInt32(Console.ReadLine());
+                e[i].Date.Date = d;
+
+                Console.Write("Enter month of the birth : ");
+                m = Convert.ToInt32(Console.ReadLine());
+                e[i].Date.Month = m;
+
+                Console.Write("Enter year for the birth : ");
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                e[i].Date.Year = y;
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
+
+/*
+ * Output :
+ 
+Create a nested structure and store data in an array :
+--------------------------------------------------------
+Enter Total no of employee you want to enter
+1
+Name of the employee : nikitha
+Enter day of the birth : 28
+Enter month of the birth : 05
+Enter year for the birth : 2000
+ */
---
+++ Assignment3/Two_Books.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 6. Write a program in C# Sharp to insert the information of two books. 
+
+Test Data :
+
+Insert the information of two books :                    
+-----------------------------------------                
+Information of book 1 :                                  
+Input name of the book : BASIC                           
+Input the author : S.TROELSTRA                           
+                                                         
+Information of book 2 :                                  
+Input name of the book : C+                              
+Input the author : G.RTRTG   
+
+                            
+Expected Output:
+
+1: Title = BASIC,  Author = S.TROELSTRA                  
+                                                         
+2: Title = C+,  Author = G.RTRTG 
+
+ */
+
+namespace Assignment3
+{
+    struct Book
+    {
+        public string name;
+        public string author;
+    }
+    class Two_Books
+    {
+        public static void Main()
+        {
+            int b = 10;
+            Book[] books = new Book[b];
+            int i, j, n = 1, k = 1;
+            Console.WriteLine("Insert The Information of Two Books.");
+            for (j = 0; j <= n; j++)
+            {
+                Console.WriteLine("Information of book : {0} ", k);
+
+                Console.Write("Input name of the book : ");
+                books[j].name = Console.ReadLine();
+
+                Console.Write("Input the author : ");
+                books[j].author = Console.ReadLine();
+                k++;
+                Console.WriteLine();
+            }
+            for (i = 0; i <= n; i++)
+            {
+                Console.WriteLine($"{i + 1}: Title = {books[i].name}, Author = {books[i].author}");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+/*
+ Output:
+
+Insert The Information of Two Books.
+Information of book : 1
+Input name of the book : BASIC
+Input the author : S.TROELSTRA
+
+Information of book : 2
+Input name of the book : c+
+Input the author : G.RTRTG
+
+1: Title = BASIC,  Author = S.TROELSTRA
+
+2: Title = c+,  Author = G.RTRTG
+*/
